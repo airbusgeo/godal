45a3c94b84c0254cec751d8e38e7b36e05129eba
Thomas Bonfort <thomas.bonfort@airbus.com>
2021-02-06 16:44:40 +0100

    finish

--- doc_test.go
+++ doc_test.go
@@ -110,17 +110,17 @@ func ExampleBand_IO() {
 	_ = ds.Close()
 }
 
-// Example_tutorial_raster_1 is godal port of the official gdal raster tutorial
+// This is the godal port of the official gdal raster tutorial
 // located at https://gdal.org/tutorials/raster_api_tut.html .
-func Example_tutorial_raster_1() {
+func Example_tutorial1() {
 	/*
-			GDALDatasetH  hDataset;
-		    GDALAllRegister();
-		    hDataset = GDALOpen( pszFilename, GA_ReadOnly );
-		    if( hDataset == NULL )
-		    {
-		        ...;
-		    }
+		GDALDatasetH  hDataset;
+		GDALAllRegister();
+		hDataset = GDALOpen( pszFilename, GA_ReadOnly );
+		if( hDataset == NULL )
+		{
+			...;
+		}
 	*/
 	godal.RegisterAll()
 	hDataset, err := godal.Open("testdata/test.tif")
@@ -128,25 +128,26 @@ func Example_tutorial_raster_1() {
 		panic(err)
 	}
 
-	/* not implemented
-	hDriver = GDALGetDatasetDriver( hDataset );
-	printf( "Driver: %s/%s\n",
+	/*
+		hDriver = GDALGetDatasetDriver( hDataset );
+		printf( "Driver: %s/%s\n",
 			GDALGetDriverShortName( hDriver ),
 			GDALGetDriverLongName( hDriver ) );
 	*/
+	//not implemented
 
 	/*
-			printf( "Size is %dx%dx%d\n",
-		        GDALGetRasterXSize( hDataset ),
-		        GDALGetRasterYSize( hDataset ),
-		        GDALGetRasterCount( hDataset ) );
+		printf( "Size is %dx%dx%d\n",
+			GDALGetRasterXSize( hDataset ),
+			GDALGetRasterYSize( hDataset ),
+			GDALGetRasterCount( hDataset ) );
 	*/
 	structure := hDataset.Structure()
 	fmt.Printf("Size is %dx%dx%d\n", structure.SizeX, structure.SizeY, structure.NBands)
 
 	/*
 		if( GDALGetProjectionRef( hDataset ) != NULL )
-		    printf( "Projection is `%s'\n", GDALGetProjectionRef( hDataset ) );
+			printf( "Projection is '%s'\n", GDALGetProjectionRef( hDataset ) );
 	*/
 	if pj := hDataset.Projection(); pj != "" {
 		fmt.Printf("Projection is '%s'\n", pj)
@@ -185,13 +186,15 @@ func Example_tutorial_raster_1() {
 		bandStructure.BlockSizeX, bandStructure.BlockSizeY,
 		bandStructure.DataType,
 		band.ColorInterp().Name())
-	/* not implemented
-	adfMinMax[0] = GDALGetRasterMinimum( hBand, &bGotMin );
-	adfMinMax[1] = GDALGetRasterMaximum( hBand, &bGotMax );
-	if( ! (bGotMin && bGotMax) )
-		GDALComputeRasterMinMax( hBand, TRUE, adfMinMax );
-	printf( "Min=%.3fd, Max=%.3f\n", adfMinMax[0], adfMinMax[1] );
+
+	/*
+		adfMinMax[0] = GDALGetRasterMinimum( hBand, &bGotMin );
+		adfMinMax[1] = GDALGetRasterMaximum( hBand, &bGotMax );
+		if( ! (bGotMin && bGotMax) )
+			GDALComputeRasterMinMax( hBand, TRUE, adfMinMax );
+		printf( "Min=%.3fd, Max=%.3f\n", adfMinMax[0], adfMinMax[1] );
 	*/
+	//not implemented
 
 	/*
 		if( GDALGetOverviewCount(hBand) > 0 )
@@ -232,10 +235,103 @@ func Example_tutorial_raster_1() {
 		panic(err)
 	}
 
+	/*
+		const char *pszFormat = "GTiff";
+		GDALDriverH hDriver = GDALGetDriverByName( pszFormat );
+		char **papszMetadata;
+		if( hDriver == NULL )
+		    exit( 1 );
+		papszMetadata = GDALGetMetadata( hDriver, NULL );
+		if( CSLFetchBoolean( papszMetadata, GDAL_DCAP_CREATE, FALSE ) )
+		    printf( "Driver %s supports Create() method.\n", pszFormat );
+		if( CSLFetchBoolean( papszMetadata, GDAL_DCAP_CREATECOPY, FALSE ) )
+		    printf( "Driver %s supports CreateCopy() method.\n", pszFormat );
+	*/
+
+	hDriver, ok := godal.RasterDriver("Gtiff")
+	if !ok {
+		panic("Gtiff not found")
+	}
+	md := hDriver.Metadatas()
+	if md["DCAP_CREATE"] == "YES" {
+		fmt.Printf("Driver GTiff supports Create() method.\n")
+	}
+	if md["DCAP_CREATECOPY"] == "YES" {
+		fmt.Printf("Driver Gtiff supports CreateCopy() method.\n")
+	}
+
+	/*	GDALDataset *poSrcDS = (GDALDataset *) GDALOpen( pszSrcFilename, GA_ReadOnly );
+		GDALDataset *poDstDS;
+		char **papszOptions = NULL;
+		papszOptions = CSLSetNameValue( papszOptions, "TILED", "YES" );
+		papszOptions = CSLSetNameValue( papszOptions, "COMPRESS", "PACKBITS" );
+		poDstDS = poDriver->CreateCopy( pszDstFilename, poSrcDS, FALSE,
+										papszOptions, GDALTermProgress, NULL );
+		if( poDstDS != NULL )
+			GDALClose( (GDALDatasetH) poDstDS );
+		CSLDestroy( papszOptions );
+
+		GDALClose( (GDALDatasetH) poSrcDS );
+	*/
+	poSrcDS, _ := godal.Open("testdata/test.tif")
+	pszDstFilename := "/vsimem/tempfile.tif"
+	defer godal.VSIUnlink(pszDstFilename)
+	//godal doesn't expose createCopy directly, but the same result can be obtained with Translate
+	poDstDS, err := poSrcDS.Translate(pszDstFilename, nil, godal.CreationOption("TILED=YES", "COMPRESS=PACKBITS"), godal.GTiff)
+	if err != nil {
+		panic(err)
+	}
+	err = poDstDS.Close()
+	if err != nil {
+		panic(err)
+	}
+	_ = poSrcDS.Close()
+
+	/*
+		GDALDataset *poDstDS;
+		char **papszOptions = NULL;
+		poDstDS = poDriver->Create( pszDstFilename, 512, 512, 1, GDT_Byte,
+									papszOptions );
+		double adfGeoTransform[6] = { 444720, 30, 0, 3751320, 0, -30 };
+		OGRSpatialReference oSRS;
+		char *pszSRS_WKT = NULL;
+		GDALRasterBand *poBand;
+		GByte abyRaster[512*512];
+		poDstDS->SetGeoTransform( adfGeoTransform );
+		oSRS.SetUTM( 11, TRUE );
+		oSRS.SetWellKnownGeogCS( "NAD27" );
+		oSRS.exportToWkt( &pszSRS_WKT );
+		poDstDS->SetProjection( pszSRS_WKT );
+		CPLFree( pszSRS_WKT );
+		poBand = poDstDS->GetRasterBand(1);
+		poBand->RasterIO( GF_Write, 0, 0, 512, 512,
+						abyRaster, 512, 512, GDT_Byte, 0, 0 );
+		GDALClose( (GDALDatasetH) poDstDS );
+	*/
+
+	//TODO: deal with error handling
+	poDstDS, err = godal.Create(godal.GTiff, pszDstFilename, 1, godal.Byte, 512, 512)
+	defer poDstDS.Close() //Close can be defered / called more than once (second+ calls are no-ops)
+
+	err = poDstDS.SetGeoTransform([6]float64{444720, 30, 0, 3751320, 0, -30})
+
+	//SetUTM and SetWellKnownGeogCS not implemented. godal allows populating
+	// a SpatialRef from a WKT or PROJ4 string, or an epsg code
+	sr, err := godal.NewSpatialRefFromEPSG(4326)
+	defer sr.Close()
+	err = poDstDS.SetSpatialRef(sr)
+
+	abyRaster := make([]byte, 512*512)
+	// ... now populate with data
+	err = poDstDS.Bands()[0].Write(0, 0, abyRaster, 512, 512)
+	err = poDstDS.Close()
+
 	// Output:
 	// Size is 10x10x3
 	// Projection is 'GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AXIS["Latitude",NORTH],AXIS["Longitude",EAST],AUTHORITY["EPSG","4326"]]'
 	// Origin = (45.000000,35.000000)
 	// Pixel Size = (1.000000,-1.000000)
 	// Block=256x256 Type=Byte, ColorInterp=Red
+	// Driver GTiff supports Create() method.
+	// Driver Gtiff supports CreateCopy() method.
 }
