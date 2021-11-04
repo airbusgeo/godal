d5f8bf8f59720a41e5b0585b04ed90dad3312494
Thomas Bonfort <thomas.bonfort@airbus.com>
2021-11-04 11:22:06 +0100

    gdal 3.4 update (#54)

--- .github/workflows/build-gdal.sh
+++ .github/workflows/build-gdal.sh
@@ -2,7 +2,7 @@
 
 GDALVERSION=$1
 
-apt update && apt-get install -y libproj-dev libgeos-dev build-essential libsqlite3-dev curl pkgconf libjpeg-turbo8-dev sqlite3
+apt update && apt-get install -y autoconf libproj-dev libgeos-dev build-essential libsqlite3-dev curl pkgconf libjpeg-turbo8-dev sqlite3
 cd $HOME
 mkdir -p gdal
 cd gdal
@@ -10,7 +10,9 @@ cd gdal
 curl -sL https://github.com/OSGeo/gdal/archive/$GDALVERSION.tar.gz -o gdal.tar.gz
 mkdir gdal
 tar  xzf gdal.tar.gz -C gdal --strip-components 1
-cd gdal/gdal
+cd gdal
+if [ -d gdal ]; then cd gdal; fi
+if [ ! -f configure ]; then ./autogen.sh; fi
 ./configure --prefix=/optgdal \
 	--enable-shared \
 	--disable-static \
