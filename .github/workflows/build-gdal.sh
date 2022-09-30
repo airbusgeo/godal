8d11f825ea55e7bff531519f7b83be69bd44b456
Thomas Bonfort <thomas.bonfort@airbus.com>
2022-09-30 18:20:34 +0200

    fixup

--- .github/workflows/build-gdal.sh
+++ .github/workflows/build-gdal.sh
@@ -15,66 +15,67 @@ cd gdal
 if [ -d gdal ]; then cd gdal; fi
 
 if [ -f CMakeLists.txt ]; then
-mkdir build
-cd build
-cmake .. \
-    -DCMAKE_INSTALL_PREFIX=/optgdal \
-    -DOGR_BUILD_OPTIONAL_DRIVERS=OFF \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DBUILD_TESTING=OFF \
-    -DGDAL_USE_CURL=OFF \
-    -DGDAL_USE_SQLITE3=OFF \
-    -DGDAL_USE_TIFF_INTERNAL=ON \
-    -DBUILD_PYTHON_BINDINGS=OFF \
-    -DENABLE_GNM=OFF \
-    -DGDAL_USE_XERCESC=OFF \
-	 -DGDAL_USE_GEOS=ON
-make -j8
-make install
-cd /build && rm -rf gdal
+	mkdir build
+	cd build
+	cmake .. \
+		-DCMAKE_INSTALL_PREFIX=/optgdal \
+		-DOGR_BUILD_OPTIONAL_DRIVERS=OFF \
+		-DGDAL_BUILD_OPTIONAL_DRIVERS=OFF \
+		-DCMAKE_BUILD_TYPE=Release \
+		-DBUILD_TESTING=OFF \
+		-DGDAL_USE_CURL=OFF \
+		-DGDAL_USE_SQLITE3=OFF \
+		-DGDAL_USE_TIFF_INTERNAL=ON \
+		-DBUILD_PYTHON_BINDINGS=OFF \
+		-DENABLE_GNM=OFF \
+		-DGDAL_USE_XERCESC=OFF \
+		-DGDAL_USE_GEOS=ON \
+		-DGDAL_USE_OGCAPI=OFF
 
+	make -j8
+	make install
 else
-if [ ! -f configure ]; then ./autogen.sh; fi
-./configure --prefix=/optgdal \
-	--enable-shared \
-	--disable-static \
-	--disable-all-optional-drivers \
-	--with-geos \
-	--with-jpeg \
-	--with-libtiff=internal \
-	--with-geotiff=internal \
-	--without-crypto \
-	--without-cryptopp \
-	--without-gnm \
-	--without-qhull \
-	--without-sqlite3 \
-	--without-pcidsk \
-	--without-lerc  \
-	--without-gif \
-	--without-pcraster \
-	--without-curl \
-	--without-png \
-	--without-tiledb \
-	--without-odbc \
-	--without-freexl \
-	--without-pcre \
-	--without-libkml \
-	--without-xml2 \
-	--without-expat \
-	--without-xerces \
-	--without-lerc \
-	--without-pg \
-	--without-curl \
-	--without-openjpeg \
-	--without-netcdf \
-	--without-hdf5 \
-	--without-hdf4 \
-	--without-ogdi \
-	--without-exr \
-	--without-spatialite
+	if [ ! -f configure ]; then ./autogen.sh; fi
+	./configure --prefix=/optgdal \
+		--enable-shared \
+		--disable-static \
+		--disable-all-optional-drivers \
+		--with-geos \
+		--with-jpeg \
+		--with-libtiff=internal \
+		--with-geotiff=internal \
+		--without-crypto \
+		--without-cryptopp \
+		--without-gnm \
+		--without-qhull \
+		--without-sqlite3 \
+		--without-pcidsk \
+		--without-lerc  \
+		--without-gif \
+		--without-pcraster \
+		--without-curl \
+		--without-png \
+		--without-tiledb \
+		--without-odbc \
+		--without-freexl \
+		--without-pcre \
+		--without-libkml \
+		--without-xml2 \
+		--without-expat \
+		--without-xerces \
+		--without-lerc \
+		--without-pg \
+		--without-curl \
+		--without-openjpeg \
+		--without-netcdf \
+		--without-hdf5 \
+		--without-hdf4 \
+		--without-ogdi \
+		--without-exr \
+		--without-spatialite
 
-make -j4
-make install
+	make -j4
+	make install
 fi
 cd $HOME
 rm -rf gdal
