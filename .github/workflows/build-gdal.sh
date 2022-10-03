ca1d7fdcc06723059c90b161d107e63c7b8d3053
Thomas Bonfort <thomas.bonfort@airbus.com>
2022-10-03 11:56:49 +0200

    try to fix failing gh actions (#103)
    Make actions work again, even though caching does not seem to work anymore

--- .github/workflows/build-gdal.sh
+++ .github/workflows/build-gdal.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+set -e
 
 GDALVERSION=$1
 
-apt update && apt-get install -y autoconf libproj-dev libgeos-dev build-essential libsqlite3-dev curl pkgconf libjpeg-turbo8-dev sqlite3
+apt update && apt-get install -y cmake autoconf libproj-dev libgeos-dev build-essential libsqlite3-dev curl pkg-config libjpeg-turbo8-dev sqlite3
 cd $HOME
 mkdir -p gdal
 cd gdal
@@ -12,47 +13,73 @@ mkdir gdal
 tar  xzf gdal.tar.gz -C gdal --strip-components 1
 cd gdal
 if [ -d gdal ]; then cd gdal; fi
-if [ ! -f configure ]; then ./autogen.sh; fi
-./configure --prefix=/optgdal \
-	--enable-shared \
-	--disable-static \
-	--disable-all-optional-drivers \
-	--with-geos \
-	--with-jpeg \
-	--with-libtiff=internal \
-	--with-geotiff=internal \
-	--without-crypto \
-	--without-cryptopp \
-	--without-gnm \
-	--without-qhull \
-	--without-sqlite3 \
-	--without-pcidsk \
-	--without-lerc  \
-	--without-gif \
-	--without-pcraster \
-	--without-curl \
-	--without-png \
-	--without-tiledb \
-	--without-odbc \
-	--without-freexl \
-	--without-pcre \
-	--without-libkml \
-	--without-xml2 \
-	--without-expat \
-	--without-xerces \
-	--without-lerc \
-	--without-pg \
-	--without-curl \
-	--without-openjpeg \
-	--without-netcdf \
-	--without-hdf5 \
-	--without-hdf4 \
-	--without-ogdi \
-	--without-exr \
-	--without-spatialite
 
-make -j4
-make install
+if [ -f CMakeLists.txt ]; then
+	mkdir build
+	cd build
+	cmake .. \
+		-UGDAL_ENABLE_DRIVER_* -UOGR_ENABLE_DRIVER_* \
+		-DOGR_BUILD_OPTIONAL_DRIVERS=OFF \
+		-DGDAL_BUILD_OPTIONAL_DRIVERS=OFF \
+		-DGDAL_USE_JPEG=ON \
+		-DCMAKE_INSTALL_PREFIX=/optgdal \
+		-DCMAKE_BUILD_TYPE=Release \
+		-DBUILD_TESTING=OFF \
+		-DGDAL_USE_CURL=OFF \
+		-DGDAL_USE_SQLITE3=OFF \
+		-DGDAL_USE_TIFF_INTERNAL=ON \
+		-DBUILD_PYTHON_BINDINGS=OFF \
+		-DENABLE_GNM=OFF \
+		-DGDAL_USE_XERCESC=OFF \
+		-DGDAL_USE_GEOS=ON \
+		-DBUILD_JAVA_BINDINGS=OFF \
+		-DBUILD_CSHARP_BINDINGS=OFF
+
+	make -j8
+	make install
+else
+	if [ ! -f configure ]; then ./autogen.sh; fi
+	./configure --prefix=/optgdal \
+		--enable-shared \
+		--disable-static \
+		--disable-all-optional-drivers \
+		--with-geos \
+		--with-jpeg \
+		--with-libtiff=internal \
+		--with-geotiff=internal \
+		--without-crypto \
+		--without-cryptopp \
+		--without-gnm \
+		--without-qhull \
+		--without-sqlite3 \
+		--without-pcidsk \
+		--without-lerc  \
+		--without-gif \
+		--without-pcraster \
+		--without-curl \
+		--without-png \
+		--without-tiledb \
+		--without-odbc \
+		--without-freexl \
+		--without-pcre \
+		--without-libkml \
+		--without-xml2 \
+		--without-expat \
+		--without-xerces \
+		--without-lerc \
+		--without-pg \
+		--without-curl \
+		--without-openjpeg \
+		--without-netcdf \
+		--without-hdf5 \
+		--without-hdf4 \
+		--without-ogdi \
+		--without-exr \
+		--without-spatialite
+
+	make -j4
+	make install
+fi
 cd $HOME
 rm -rf gdal
 rm -rf /usr/local/bin
